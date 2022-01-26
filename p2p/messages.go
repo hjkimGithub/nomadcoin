fd3f402f55515d0e7a618db4e2caca9798cd88fe
hjkimGithub <rlaguswo3408@gmail.com>
2022-01-26 17:26:23 +0900

    broadcast peer

--- p2p/messages.go
+++ p2p/messages.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	"github.com/hjkimGithub/nomadcoin/blockchain"
 	"github.com/hjkimGithub/nomadcoin/utils"
@@ -16,6 +17,7 @@ const (
 	MessageAllBlocksResponse
 	MessageNewBlockNotify
 	MessageNewTxNotify
+	MessageNewPeerNotify
 )
 
 type Message struct {
@@ -59,6 +61,11 @@ func notifyNewTx(tx *blockchain.Tx, p *peer) {
 	p.inbox <- m
 }
 
+func notifyNewPeer(address string, p *peer) {
+	m := makeMessage(MessageNewPeerNotify, address)
+	p.inbox <- m
+}
+
 func handleMsg(m *Message, p *peer) {
 	// fmt.Printf("Peer: %s, Sent a message with kind of: %d", p.key, m.Kind)
 	switch m.Kind {
@@ -91,5 +98,11 @@ func handleMsg(m *Message, p *peer) {
 		var payload *blockchain.Tx
 		utils.HandleErr(json.Unmarshal(m.Payload, &payload))
 		blockchain.Mempool().AddPeerTx(payload)
+	case MessageNewPeerNotify:
+		var payload string
+		utils.HandleErr(json.Unmarshal(m.Payload, &payload))
+		parts := strings.Split(payload, ":")
+		// parts = 127.0.0.1 : 2000 : 4000 addr: port: openPort
+		AddPeer(parts[0], parts[1], parts[2], false)
 	}
 }
